54b4f4781745a84bbd400814670254ffed4887af
Khoi Vu <khoivukha0509@gmail.com>
2023-03-27 14:35:25 +0700

    Use Flask-CORS to enable cross-domain requests and set response headers.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,13 +13,15 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
 
-    """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    """
+    # Set up CORS. Allow '*' for origins.
+    CORS(app)
 
-    """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    """
+    # Use the after_request decorator to set Access-Control-Allow
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     """
     @TODO:
