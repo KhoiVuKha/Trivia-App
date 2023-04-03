72fb0bbe588407b2b52f5538e84a23641b057a44
Khoi Vu <khoivukha0509@gmail.com>
2023-04-03 14:08:49 +0700

    Remove todo for get questions based on category

--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -65,7 +65,7 @@ class QuestionView extends Component {
 
   getByCategory = (id) => {
     $.ajax({
-      url: `/categories/${id}/questions`, //TODO: update request URL
+      url: `/categories/${id}/questions`,
       type: 'GET',
       success: (result) => {
         this.setState({
